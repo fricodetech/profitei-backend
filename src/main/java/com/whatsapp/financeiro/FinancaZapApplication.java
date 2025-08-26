7e26b32b6cd97ec47fda3c1a841c512d3b67f1ad
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-08-26 11:22:39 -0300

    Trazendo classe principal e as dependencias necessárias

---
+++ src/main/java/com/whatsapp/financeiro/FinancaZapApplication.java
@@ -0,0 +1,13 @@
+package com.whatsapp.financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancaZapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancaZapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whatsapp/financeiro/FinancaZapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatsapp.financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancaZapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
